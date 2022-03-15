2c4542b6671acac8057307c68c380ff5d6474919
Adrien Clay <adrienclay36@gmail.com>
2022-03-15 12:29:36 -0600

    Trying to hook up app bar

--- CarEvaluator/client/src/components/Navbar/mobile-nav-item.js
+++ CarEvaluator/client/src/components/Navbar/mobile-nav-item.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Link from "next/link";
+
 import styles from './mobile-nav-item.module.css'
 const MobileNavItem = (props) => {
   return (
     <>
-      <Link href={props.href} passHref>
+     
         <li
           className={`my-4 font-light tracking-widest`}
         >
           {props.children}
         </li>
-      </Link>
+  
       <hr className="border-b" />
     </>
   );
--- CarEvaluator/client/src/components/Navbar/navbar.js
+++ CarEvaluator/client/src/components/Navbar/navbar.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import MobileNav from "./mobile-nav";
-
+import { Burger } from "@mantine/core";
 import styles from "./navbar.module.css";
-
+import { useMediaQuery } from "@mantine/hooks";
 import SubMenu from "./sub-menu";
 const Navbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isInitial, setIsInitial] = useState(true);
-  const router = useRouter();
   const hideLogo = useMediaQuery('(max-width: 1024px)');
 
   const toggleMenu = () => {
@@ -18,44 +17,44 @@ const Navbar = (props) => {
   };
   return (
     <div>
-      <SocialIcons textColor="text-white" textHover={"text-slate-200"} />
+      
       <nav
         className={`container ${props.classes} flex items-center py-4 ${
           props.marginTop && "sm:mt-12"
         }`}
       >
-        {!hideLogo && <Link href="/" passHref>
+        {/* {!hideLogo && <Link href="/" passHref>
           <div className={`py-1 ${props.invertImage && "invert"}`}>
             <LogoUnited />
           </div>
-        </Link>}
+        </Link>} */}
         <ul
           className={`hidden sm:flex flex-1 flex-wrap lg:flex-nowrap md:flex-wrap justify-center lg:justify-end md:justify-center items-center gap-6 lg:gap-10 md:gap-6 uppercase text-sm mt-2 text-semibold overflow-hidden ${props.textColor}`}
         >
           {navLinks.map((link) => {
             if (!link.subNav) {
               return (
-                <Link key={link.text} href={link.href} passHref>
+               
                   <li className="mb-2 cursor-pointer tracking-widest hover:text-slate-200 text-center">
                     {link.text}
                   </li>
-                </Link>
+              
               );
             }
-            if (link.subNav) {
-              return (
-                <SubMenu
-                  size={link.size}
-                  key={link.text}
-                  control={
-                    <button onClick={() => router.push(link.href)} className="mb-2 cursor-pointer tracking-widest hover:text-slate-200 text-center uppercase">
-                      {link.text}
-                    </button>
-                  }
-                  items={link.items}
-                />
-              );
-            }
+            // if (link.subNav) {
+            //   return (
+            //     <SubMenu
+            //       size={link.size}
+            //       key={link.text}
+            //       control={
+            //         <button onClick={() => router.push(link.href)} className="mb-2 cursor-pointer tracking-widest hover:text-slate-200 text-center uppercase">
+            //           {link.text}
+            //         </button>
+            //       }
+            //       items={link.items}
+            //     />
+            //   );
+            // }
           })}
         </ul>
         <div className="flex sm:hidden flex-1 justify-end mr-3">
