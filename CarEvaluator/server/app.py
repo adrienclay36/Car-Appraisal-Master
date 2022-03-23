f9986ca8b6e9caa231f54029447106a253e7001b
Adrien Clay <adrienclay36@gmail.com>
2022-03-23 09:16:49 -0600

    Receiving data on flask server, fixed CORS policy

--- CarEvaluator/server/app.py
+++ CarEvaluator/server/app.py
@@ -1,7 +1,13 @@
+from dataclasses import replace
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 from xgboost import XGBRegressor
 import sys
+from flask_cors import CORS
+
+CORS(app, resources={r'/*': { 'origins': ['*']}})
+
+
 
 MODEL = XGBRegressor()
 MODEL.load_model('./linear_model.json')
@@ -14,12 +20,25 @@ def load_server():
 
 @app.route("/evaluate", methods=["POST"])
 def evaluate_car():
-    
-    print("Test", file=sys.stdout)
-    input_data = request.json()
-    print(input_data, file=sys.stderr)
 
-    return jsonify({ 'status': 200, 'message': 'Data Received'})
+    input_data = request.get_json()
+   
+
+    replace_booleans = {}
+    for key, value in input_data.items():
+        replace_booleans[key] = value
+
+        if(value == True):
+            replace_booleans[key] = 1
+        if(value == False):
+            replace_booleans[key] = 0
+    
+    print(replace_booleans, file=sys.stderr)
+            
+  
+
+
+    return jsonify({ 'status': '200', 'message': 'Data Received' })
 
 
 if __name__ == '__main__':
