cddf14912b24c309e38d5c9e6fc0ca0b9ae805fd
Adrien Clay <adrienclay36@gmail.com>
2022-04-01 10:12:32 -0600

    setting up gunicorn for heroku deployment

--- CarEvaluator/server/app.py
+++ CarEvaluator/server/app.py
@@ -1,46 +1,46 @@
+from flask_cors import CORS
+import sys
+from xgboost import XGBRegressor
 from dataclasses import replace
 from flask import Flask, request, jsonify
 import pandas as pd
 app = Flask(__name__)
-from xgboost import XGBRegressor
-import sys
-from flask_cors import CORS
 
-CORS(app, resources={r'/*': { 'origins': ['*']}})
+CORS(app, resources={r'/*': {'origins': ['*']}})
 
 COLUMN_ORDER = ['mileage',
- 'Bluetooth',
- 'Backup Camera',
- 'Infotainment',
- 'Screen',
- 'Navigation',
- 'Hands Free Calling',
- 'Heated Seats',
- 'Power Seat Controls',
- 'Rear Air Vents',
- 'Bed Liner',
- 'Tow Hitch',
- 'city_mpg',
- 'highway_mpg',
- 'year',
- 'model_cat',
- 'make_cat']
+                'Bluetooth',
+                'Backup Camera',
+                'Infotainment',
+                'Screen',
+                'Navigation',
+                'Hands Free Calling',
+                'Heated Seats',
+                'Power Seat Controls',
+                'Rear Air Vents',
+                'Bed Liner',
+                'Tow Hitch',
+                'city_mpg',
+                'highway_mpg',
+                'year',
+                'model_cat',
+                'make_cat']
 
 MODEL = XGBRegressor()
-MODEL.load_model('./linear_model.json')
+MODEL.load_model('./static/linear_model.json')
 MODEL_RMSE = 3541
+
+
 @app.route("/")
 def load_server():
     data = {'status': 200, 'message': 'Server Ready to accept data'}
     return jsonify(data)
 
 
-
 @app.route("/evaluate", methods=["POST"])
 def evaluate_car():
 
     input_data = request.get_json()
-   
 
     replace_booleans = {}
     for key, value in input_data.items():
@@ -54,17 +54,12 @@ def evaluate_car():
     replace_booleans['make_cat'] = int(replace_booleans['make_cat'])
     replace_booleans['model_cat'] = int(replace_booleans['model_cat'])
 
-
-    
     input_df = pd.DataFrame(replace_booleans, index=[0])
 
     input_df = input_df[COLUMN_ORDER]
     prediction = MODEL.predict(input_df)[0]
-            
-  
 
-
-    return jsonify({ 'status': '200', 'message': 'Data Received', 'prediction': str(prediction), 'error': MODEL_RMSE })
+    return jsonify({'status': '200', 'message': 'Data Received', 'prediction': str(prediction), 'error': MODEL_RMSE})
 
 
 if __name__ == '__main__':
